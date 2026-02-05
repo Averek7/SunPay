b525e5a0aa7d707acbcac95ea37b9096f74332fc
Avanish Singh <singhavanish2003@gmail.com>
2026-02-05 11:03:30 +0530

    Homepage + Wallet Done Onto Dashboard

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { useMemo } from 'react';
+import { AppProps } from "next/app";
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const network = useMemo(() => "devnet" as const, []);
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
